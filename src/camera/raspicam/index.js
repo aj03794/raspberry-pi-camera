9cffa479b361fabdc564062a49a02c3110f230d5
axj1l59 <adam_johnston1@homedepot.com>
2018-04-15 19:46:10 -0400

    Functionality to take photo

--- src/camera/raspicam/index.js
+++ src/camera/raspicam/index.js
@@ -1,6 +1,6 @@
 // import RaspiCam from 'raspicam'
-// import { resolve as resolvePath } from 'path'
-
+import { resolve as resolvePath } from 'path'
+import { exec } from 'child_process'
 
 // //
 // // //to take a snapshot, start a timelapse or video recording
@@ -29,74 +29,23 @@
 // // 	console.log('')
 // // });
 
-// export const raspicam = () => {
-
-// 	// const camera = true
-// 	// console.log(resolvePath(__dirname, 'pictures'))
-
-// 	// const photoLocation = resolvePath()
-// 	const opts = {
-// 		mode: 'photo',
-// 		// Will need to make a folder that houses the pictures
-// 		output: './image.jpg'
-// 	}
-
-// 	const camera = new RaspiCam(opts)
-// 	console.log('camera', camera)
-//     camera.start( )
-//     camera.stop( )
-// 	return {
-// 		takePhoto: () => new Promise((resolve, reject) => {
-// 			console.log('Taking photo')
-// 			// camera.start( );
-//             // camera.stop( );
-// 			// camera.on("stop", () => {
-// 			// 	console.log('Finished uploading')
-// 			// 	resolve({
-// 			// 		photoLocation: resolvePath(__dirname, 'pictures')
-// 			// 	})
-// 			// })
-//             return resolve()
-// 			// return resolve({
-// 			// 	photoLocation: resolvePath(__dirname, 'pictures')
-// 			// })
-// 		})
-// 	}
-// }
+export const raspicam = () => {
 
 
-var RaspiCam = require("raspicam");
-
-var camera = new RaspiCam({
-    mode: 'photo',
-    output: './image.jpg'
-});
-
-//to take a snapshot, start a timelapse or video recording
-camera.start( );
-
-//to stop a timelapse or video recording
-
-//listen for the "start" event triggered when the start method has been successfully initiated
-camera.on("start", function(){
-	//do stuff
-    console.log('starting')
-});
-
-//listen for the "read" event triggered when each new photo/video is saved
-camera.on("read", function(err, timestamp, filename){ 
-	//do stuff
-    console.log('filename', filename)
-});
-
-//listen for the "stop" event triggered when the stop method was called
-camera.on("stop", function(){
-	//do stuff
-});
-
-//listen for the process to exit when the timeout has been reached
-camera.on("exit", function(){
-	//do stuff
-});
-
-camera.stop( );
+return {
+    takePhoto: () => new Promise((resolve, reject) => {
+        exec(`raspistill -o cam.jpg`,
+    		{
+    			cwd: resolvePath(__dirname)
+    		},
+    		(err, stdout, stderr) => {
+    			if (err) {
+    				console.log('Something went wrong', err)
+    				return reject({ message: 'Something went wrong uploading' })
+    			}
+    			console.log(stdout)
+    			return resolve({ msg: 'Picture taken successfully' })
+    		}
+    	)}
+    })
+}
