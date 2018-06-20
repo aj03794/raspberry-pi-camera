389a186aa456daf0a58690f16fdca89021cc63fd
aj03794 <adam_johnston@homedepot.com>
2018-06-20 19:01:18 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import { getSetting } from './settings'
 import { slack as createSlack } from './slack'
 import { manageFolder } from './manage-folder'
 
-const pubsubProvider = getSetting('pubsub')
+// const pubsubProviders = getSetting('pubsub')
+// const pubsubProviderLength = pubsubProvider.length
 const cameraProvider = getSetting('camera')
 
-console.log('New redis setup')
-console.log('process', process.argv[2])
-console.log(process.argv[2] === 'dev' ? '127.0.0.1' : 'main.local')
 console.log('UUID ----->', process.env.UUID || 'dev')
 
-console.log('pubsubProvider', pubsubProvider)
-console.log('cameraProvider', cameraProvider)
+// console.log('pubsubProvider', pubsubProvider)
+// console.log('cameraProvider', cameraProvider)
+
+// getSetting('pubsub').map(x => console.log('X', x))
+
+const pubsubProviders = getSetting('pubsub').map(pubsubProvider => {
+    console.log('pubsubProvider', pubsubProvider)
+    const x = import(`./pub-sub`)
+    console.log('X', x)
+    return x
+})
+Promise.all(pubsubProviders).then(result => console.log('RESULT', result))
 
 const uuid = process.env.UUID || 'dev'
 
@@ -21,41 +29,43 @@ const imports = [
     import(`./pub-sub/gcp`)
 ]
 
-Promise.all(imports)
-.then(([
-    { [pubsubProvider]: pubsub },
-    { [cameraProvider]: camera },
-    { gcp }
-]) => {
-
-    const { publisherCreator, subscriberCreator } = pubsub({
-        host: process.argv[2] === 'dev' ? '127.0.0.1' : 'main.local'
-    })
-    return Promise.all([
-        publisherCreator(),
-        subscriberCreator(),
-        gcp({ getSetting })
-    ])
-    .then(([
-        { publish },
-        { subscribe },
-        { allGcpMsgs, filterGcpMsgs }
-    ]) => {
-        const slack = createSlack({ publish })
-        const gcpFunctions = {
-            allGcpMsgs,
-            filterGcpMsgs,
-        }
-        const pubsubFunctions = {
-            publish,
-            subscribe
-        }
-        return camera({
-            ...pubsubFunctions,
-            getSetting,
-            slack,
-            manageFolder,
-            ...gcpFunctions
-        })
-    })
-})
+// Promise.all(imports)
+// .then(([
+    // { [pubsubProviders[0]]:  },
+    // { [pubsubProvider[1]]: pubsub },
+    // pubsub,
+//     { [cameraProvider]: camera },
+//     { gcp }
+// ]) => {
+//     console.log('PUBSUB', one)
+//     const { publisherCreator, subscriberCreator } = pubsub({
+//         host: process.argv[2] === 'dev' ? '127.0.0.1' : 'main.local'
+//     })
+//     return Promise.all([
+//         publisherCreator(),
+//         subscriberCreator(),
+//         gcp({ getSetting })
+//     ])
+//     .then(([
+//         { publish },
+//         { subscribe },
+//         { allGcpMsgs, filterGcpMsgs }
+//     ]) => {
+//         const slack = createSlack({ publish })
+//         const gcpFunctions = {
+//             allGcpMsgs,
+//             filterGcpMsgs,
+//         }
+//         const pubsubFunctions = {
+//             publish,
+//             subscribe
+//         }
+//         return camera({
+//             ...pubsubFunctions,
+//             getSetting,
+//             slack,
+//             manageFolder,
+//             ...gcpFunctions
+//         })
+//     })
+// })
