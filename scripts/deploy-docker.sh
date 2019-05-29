083325db1395a879fc0c758fc2c064938d0442de
axj1l59 <adam_johnston1@homedepot.com>
2019-05-29 08:07:45 -0400

    WIP

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -3,8 +3,8 @@ IMAGE=raspberry-pi-camera
 # ensure we're up to date
 # git pull
 # bump version
-# version=$(npm version patch -m "v%s [ci skip]")
-# version=$(echo $version | cut -c 2-)
+version=$(npm version patch -m "v%s [ci skip]")
+version=$(echo $version | cut -c 2-)
 echo "version: $version"
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker tag $DOCKER_USERNAME/$IMAGE:latest $DOCKER_USERNAME/$IMAGE:$version
